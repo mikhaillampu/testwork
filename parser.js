8b1db831df420d7062604c7b221fd2c056494526
mikhaillampu <77235933+mikhaillampu@users.noreply.github.com>
2021-09-14 12:02:51 +0300

    db->db()

--- parser.js
+++ parser.js
@@ -7,10 +7,10 @@ module.exports = async function getCurrencyRates(fsyms, tsyms) {
                 if (fsyms && tsyms) {
                         return await requester(fsyms, tsyms)
                 } else if (!fsyms && !tsyms) {
-                        await db.authenticate();
+                        await db().authenticate();
                         for await (const currency of fsyms) {
                                 const currencySnapShot = await requester(currency, tsyms);
-                                await db.create({ currency, currencySnapShot })
+                                await db().create({ currency, currencySnapShot })
                         }
                 } else return { error: 'You should provide both fsyms and tsyms or nothing as arguments' }
         }
@@ -24,7 +24,7 @@ async function requester(fsyms, tsyms) {
                 return (await axios.get(`${source}fsyms=${fsyms}&tsyms=${typeof (tsyms) === 'string' ? tsyms : tsyms.join(',')}`)).data
         }
         catch (error) {
-                return await db.findOne({ where: { currency: fsyms }, order: [['createdAt', 'DESC']] })
+                return await db().findOne({ where: { currency: fsyms }, order: [['createdAt', 'DESC']] })
         }
 
 };
